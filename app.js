8ff325a0ecfccddce45995d5ebae86a9295d9753
Toru, Kobayashi <koba0004@gmail.com>
2013-03-28 15:59:45 +0900

    do not use express, only connect

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express'),
-    app = express(),
-    http = require('http'),
-    server = http.createServer(app),
-    io = require('socket.io').listen(server);
+var connect = require('connect'),
+    app     = connect(),
+    http    = require('http'),
+    server  = http.createServer(app),
+    io      = require('socket.io').listen(server);
 
 // http response
 server.listen(3000);
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/public/index.html');
+app.use(connect.static('public'));
+app.use(function(req, res) {
+  res.sendfile('public/index.html');
 });
 
 // socket.io
